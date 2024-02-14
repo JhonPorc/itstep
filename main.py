1ee87d251ad228df98912544bc5c0309999c3be8
JhonPorc <mifodikorjevo@gmail.com>
2024-02-14 17:44:17 +0200

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello")
+class Student:
+    print("Hi")
+    def __init__(self):
+        self.height = 160
+        print("I am alive")
+
+first_student = Student()
+
+class Student:
+    def __init__(self, height=160):
+        self.height = height
+
+nick = Student()
+kate = Student(height=170)
+Chiril = Student(height=2033)
+print(nick.height)
+print(kate.height)
+print(Chiril.height)
+
+
